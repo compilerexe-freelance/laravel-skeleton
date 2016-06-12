79ca0115416c934b26eb097e5804bbbeb651dcd7
Me <compilerexe@gmail.com>
2016-06-12 20:02:29 +0700

    Init

---
+++ laravel/resources/views/master.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <title>Laravel</title>
+
+    <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+  </head>
+  <body>
+
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a href="{!! URL('/') !!}" class="navbar-brand">Laravel</a>
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#" class="active">Home</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  </body>
+</html>
